1b3c545a5cd054db742778eaaf3cf1f3611eea85
Jeremy Wiles-Young <jwilesyoung@gmail.com>
2024-05-23 10:15:27 -0400

    add fonts

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter, Dangrek } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const dang = Dangrek({
+  subsets: ['latin'], 
+  display: 'swap', 
+  weight:'400'
+});
